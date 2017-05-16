7775f9020b725d60193d6c848a0091b6028d4ad9
doublefox1981 <331847809@qq.com>
2017-05-16 17:30:23 +0800

    33

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
 curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
+mkdir -p /etc/docker
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://0oo8g0xh.mirror.aliyuncs.com"]
+}
+EOF
+systemctl daemon-reload
+systemctl restart docker
